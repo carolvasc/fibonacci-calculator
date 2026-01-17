9f53f76d21e6054421c65e65b66f810dab50dae2
Carolina Vasconcelos <cvsouza04@gmail.com>
2026-01-17 09:56:28 -0300

    config: fixing setupTests.js

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -4,6 +4,24 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
+const originalConsoleError = console.error;
+beforeAll(() => {
+  console.error = (...args) => {
+    const message = args[0];
+    if (
+      typeof message === "string" &&
+      message.includes("ReactDOMTestUtils.act is deprecated")
+    ) {
+      return;
+    }
+    originalConsoleError(...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalConsoleError;
+});
+
 jest.mock("axios", () => ({
   get: jest.fn((url) => {
     if (typeof url === "string" && url.includes("/values/all")) {
